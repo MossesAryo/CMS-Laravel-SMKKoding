9878d967bc2db0161d527c5b6ef921fcd8435d38
MossesAryo <mossesaryobimo@gmail.com>
2025-01-18 18:26:14 +0700

    Tampil Data Article & Category Di halaman Front End

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Http\Controllers\Back\CategorController;
 use App\Http\Controllers\Controller;
+use App\Models\Articles;
+use App\Models\Categories;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('front.home.index');
+        return view('front.home.index', [
+            'latest_post' => Articles::latest()->first(),
+            'articles' => Articles::latest()->get(),
+            'categories' => Categories::latest()->get()
+        ]);
     }
 }
